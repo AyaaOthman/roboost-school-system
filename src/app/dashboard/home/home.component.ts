94b75532554445822c208a9e35f783ba3294309e
Aya Othman <69984725+AyaaOthman@users.noreply.github.com>
2024-01-07 18:51:43 +0200

    get All Students

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Student } from 'src/app/interfaces/student.interface';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  constructor(private studentService: StudentService) {}
+  studentsArr: Student[] = [];
+  getStudents() {
+    this.studentService.getAllStudents().subscribe({
+      next: (res) => {
+        this.studentsArr = res.Data;
+        console.log(this.studentsArr);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+  ngOnInit() {
+    this.getStudents();
+  }
 }
--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StudentService {
+  baseUrl = 'https://api.mohamed-sadek.com/';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+  getAllStudents(): Observable<any> {
+    return this.httpClient.get(`${this.baseUrl}Student/Get`);
+  }
 }
